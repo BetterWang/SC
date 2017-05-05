dfeccfeac5e24ed5f7ede531ab6ed2a749a14b29
Quan Wang <BetterWang@gmail.com>
2017-05-05 15:30:28 +0200

    [+] label

--- label.h
+++ label.h
@@ -4,9 +4,9 @@ TChain * chV = new TChain("trV");
 
 const char * fname[] = {
 	"test/",				// 0
-	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_v1/170427_131539/0000/",		// 1 PbPb 5 TeV Pixel Cent
-	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_v1/170427_131539/0000/",		// 2 PbPb 5 TeV Pixel Cent PosEta
-	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_v1/170427_131539/0000/",		// 3 PbPb 5 TeV Pixel Cent NegEta
+	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_v1/170427_131539/0000/",			// 1 PbPb 5 TeV Pixel Cent
+	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_sysPosEta_v1/170504_142352/0000/",	// 2 PbPb 5 TeV Pixel Cent PosEta
+	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_sysNegEta_v1/170504_142426/0000/",	// 3 PbPb 5 TeV Pixel Cent NegEta
 };
 
 const char * ftxt[] = {
