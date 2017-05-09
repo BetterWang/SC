a62b51a75ac6feb2c7671f28a78188b79c91c3a6
Quan Wang <BetterWang@gmail.com>
2017-05-09 19:58:23 +0200

    [+] label

--- label.h
+++ label.h
@@ -6,7 +6,8 @@ const char * fname[] = {
 	"test/",				// 0
 	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_v1/170427_131539/0000/",			// 1 PbPb 5 TeV Pixel Cent
 	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_sysPosEta_v1/170504_142352/0000/",	// 2 PbPb 5 TeV Pixel Cent PosEta
-	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_sysNegEta_v1/170504_142426/0000/",	// 3 PbPb 5 TeV Pixel Cent NegEta
+	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_SC_Pixel_eff_cent_sysNegEta_v3/170507_181919/0000/",	// 3 PbPb 5 TeV Pixel Cent NegEta
+	"../PbPb2015_cumu/MC/HIJING_SC_mod0/",									// 4 HIJING 5 TeV Pixel Cent NegEta
 };
 
 const char * ftxt[] = {
@@ -14,6 +15,7 @@ const char * ftxt[] = {
 	"txt/PbPb5TeV_Pixel_cent/",			// 1
 	"txt/PbPb5TeV_Pixel_cent_PosEta/",		// 2
 	"txt/PbPb5TeV_Pixel_cent_NegEta/",		// 3
+	"txt/HIJING_fEllP_8pct_v2/",			// 4
 };
 
 void addchain(int s1, string s)
